13e5411b3f8a4e959023c3924ba6a36dca69e5e5
Katy Brown <katybr35@gmail.com>
2022-05-16 12:01:54 -0500

    trying to get node_modules to gitignore

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require('Express');
+const fs = require('fs')
+const path = require('path')
 
 const app = express();
+const port = process.env || 3001
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
